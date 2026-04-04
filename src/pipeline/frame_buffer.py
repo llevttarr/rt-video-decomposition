8db2d94de02dd14475df6d9f19f18b20259dc2e6
llevttarr <llevttarr@gmail.com>
2026-04-04 19:11:11 +0300

    pipeline

---
+++ src/pipeline/frame_buffer.py
@@ -0,0 +1,12 @@
+from ..core.tens.vec import Vector
+from ..core.tens.mat import Matrix
+
+class FrameBuffer:
+    def __init__(self,n:int):
+        self.n=n
+        self.frames:list[Vector]=[]
+    def push(self,frame:Vector):
+        # if (self.is_full):
+        pass
+    def is_full(self):
+        return len(self.frames)>=self.n
---
+++ src/pipeline/pipeline.py
@@ -0,0 +1,23 @@
+from ..core.tens.vec import Vector
+from ..core.tens.mat import Matrix
+
+class VideoPipeline:
+    def __init__(self):
+        pass
+    def process(self):
+        # STAGES
+        # - - -
+        # 1. get raw frame
+        # 2. preprocess
+        # 2.1. grayscale 
+        # 2.2. resize 
+        # 2.3. flatten 
+        # 3. push into buffer
+        # 3.1. if buffer small enough -> show whole frame
+        #      else -> proceed
+        # 4. compute background estimation
+        # 5. show only the foreground
+        # - - -
+        
+        
+        pass
