218ab36e45bf03092ff64ba874fd8661d04e8ba3
llevttarr <llevttarr@gmail.com>
2026-04-05 14:25:22 +0300

    incremental pca

--- src/pipeline/background_model.py
+++ src/pipeline/background_model.py
@@ -9,25 +9,50 @@ from debug.debug import dbg
 import numpy as np
 
 class BackgroundModel():
-    def __init__(self,rank:int,threshold:float):
+    def __init__(self,rank:int,threshold:float,rate=0.001):
         self.rank=rank
         self.threshold=threshold
         self.bg=None
         self.fg=None
-    def process(self,x:Matrix,v:Vector):
-        self.bg=self.estimate_background(x,v)
+        self.mean=None
+        self.u=None
+        self.rate=rate
+        self.initialized=False
+    def init_model(self,x:Matrix):
+        self.mean=np.mean(x.data,axis=1)
+        xmd=x.data-self.mean[:,None]
+        xm=Matrix(xmd)
+        u,s,v=svd_decomposition(xm)
+        self.initialized=True
+
+        r=min(u.shape[1],self.rank)
+        self.u=u[:,:r]
+        dbg("model init")
+        
+    def process(self,v:Vector):
+        x=v.data
+        z=x-self.mean
+        
+        coeff=self.u.T.data@z
+        z_bg=self.u.data@coeff
+        bg=self.mean+z_bg
+        self.bg=Vector(*bg)
         self.fg=self.get_foreground(v)
-    def estimate_background(self,x,framev):
-        u,s,v=svd_decomposition(x)
-        r=min(self.rank,u.shape[1])
-        if r<=0:
-            dbg("rank <= 0")
-            raise ValueError()
-        ur= Matrix(u.data[:, :r])
-        coeff=ur.T @ framev
-        bg=ur@coeff
-        return bg
+        self.upd_model(x,z)
     def get_foreground(self,v:Vector):
         residual = np.abs(v.data-self.bg.data)
         mask = np.where(residual>self.threshold,255.0,0.0)
         return Vector(*mask)
+    def upd_model(self,x,z,eps=1e-6):
+        rate=self.rate
+        self.mean=self.mean*(1.0-rate)+rate*x
+        coeff=self.u.T.data@z
+        z_proj=self.u.data@coeff
+        residual=z-z_proj
+        norm=np.linalg.norm(residual)
+        if norm >eps:
+            new_d=residual/norm
+            u_updat=np.column_stack([self.u.data,new_d])
+            u_upd=Matrix(u_updat)
+            q,_=qr_decomposition(u_upd)
+            self.u=q[:,:self.rank]
--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -24,32 +24,23 @@ class VideoPipeline:
         self.h=h
 
     def process(self,frame):
-        # STAGES
         # - - -
-        # 1. get raw frame
-        # 2. preprocess
-        # 2.1. grayscale 
-        # 2.2. resize 
-        # 2.3. flatten 
-        # 3. if buffer large enough -> process
-        # 3.1. compute background estimation
-        # 3.2. compute foreground
-        # 3.3. recompute result
-        # 4. push into buffer
-        # 5. output result
         # - - -
         v,vshape=self.preprocess(frame)
 
         res= PipelineResult(frame,v,vshape,None,None)
 
-        buffer=self.buffer
-        if (buffer.is_full()):
-            x:Matrix=buffer.to_mat()
-            self.model.process(x,v)
+        
+        if not self.model.initialized:
+            self.buffer.push(v)
+            if self.buffer.is_full():
+                x=self.buffer.to_mat().data
+                self.model.init_model(x)
+            return res.output(self.w, self.h)
 
-            res.background=self.model.bg
-            res.foreground_mask=self.model.fg
-        buffer.push(v)
+        self.model.process(v)
+        res.background=self.model.bg
+        res.foreground_mask=self.model.fg
         return res.output(self.w,self.h)
     def preprocess(self,frame)->tuple[Vector,tuple[int,int]]:
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
